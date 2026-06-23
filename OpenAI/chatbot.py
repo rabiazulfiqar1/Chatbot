5fcb921f1472fa82021bbd803477e9f5e5a39208
Rabia Zulfiqar <rzulfiqar889@gmail.com>
2026-06-23 16:55:56 +0500

    feat: add additional configurable params and streaming

--- OpenAI/chatbot.py
+++ OpenAI/chatbot.py
@@ -1,17 +1,22 @@
 import os
+import openai
 from openai import OpenAI
 from dotenv import load_dotenv
 import asyncio
-from openai import AsyncOpenAI #noqa
+from openai import AsyncOpenAI
 
 load_dotenv()
 
 client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),
+    max_retries=2, # configurable
+    timeout=10.0, # configurable
 )
 
-asyncClient = OpenAI(
+asyncClient = AsyncOpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),
+    max_retries=2, # configurable
+    timeout=10.0, # configurable
 )
 
 history = []
@@ -21,20 +26,45 @@ async def generate_async_response(query):
         "role": "user",
         "content": query
     })
-    stream = await asyncClient.chat.completions.create(
-        model="gpt-5.4",
-        messages=[
-            {"role": "system", "content": "Talk like a pirate."},
-            *history
-        ],
-        stream = True
-    )
+
+    try:
+        stream = await asyncClient.responses.create(
+            model="gpt-4o-mini",
+            instructions="Talk in a frinedly way.",
+            input=history,
+            stream=True
+        )
+    except openai.APIConnectionError as e:
+        print("The server could not be reached")
+        print(e.__cause__)  # an underlying Exception, likely raised within httpx.
+        return ""
+    except openai.RateLimitError:
+        print("A 429 status code was received; we should back off a bit.")
+        return ""
+    except openai.APIStatusError as e:
+        print("Another non-200-range status code was received")
+        print(e.status_code)
+        print(e.response)
+        return ""
+
+    # stream = await asyncClient.chat.completions.create(
+    #     model="gpt-4o-mini",
+    #     messages=[
+    #         {"role": "system", "content": "Talk in a frinedly way."},
+    #         *history
+    #     ],
+    #     stream = True
+    # )
 
     response =""
     async for event in stream:
         if event.type == "response.output_text.delta":
             print(event.delta, end="", flush=True)
             response += event.delta
+            
+        # if event.choices[0].delta.content:
+        #     print(event.choices[0].delta.content, end="", flush=True)
+        #     response += event.choices[0].delta.content
     
     print()
     return response
@@ -45,20 +75,34 @@ def generate_response(query):
         "role": "user",
         "content": query
     })
-    stream = client.chat.completions.create(
-        model="gpt-5.4",
-        messages=[
-            {"role": "system", "content": "Talk like a pirate."},
-            *history
-        ],
-        stream = True
-    )
+    try:
+        stream = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "system", "content": "Talk in a frinedly way."},
+                *history
+            ],
+            stream = True
+        )
+    except openai.APIConnectionError as e:
+        print("The server could not be reached")
+        print(e.__cause__)  # an underlying Exception, likely raised within httpx.
+        return ""
+    except openai.RateLimitError:
+        print("A 429 status code was received; we should back off a bit.")
+        return ""
+    except openai.APIStatusError as e:
+        print("Another non-200-range status code was received")
+        print(e.status_code)
+        print(e.response)
+        return ""
+
 
     response = ""
     for event in stream:
-        if event.type == "response.output_text.delta":
-            print(event.delta, end="", flush=True)
-            response += event.delta
+        if event.choices[0].delta.content:
+            print(event.choices[0].delta.content, end="", flush=True)
+            response += event.choices[0].delta.content
 
     print()
     return response
@@ -68,13 +112,11 @@ async def main():
         user_query = input("Enter prompt Or q to exit: ")
         if (user_query == "q"):
             break
-        response = generate_response(user_query)
+        response = await generate_async_response(user_query)
         history.append({
             "role": "assistant",
             "content": response
         })
         print(history)
 
-asyncio.run(main)
-
-
+asyncio.run(main())
